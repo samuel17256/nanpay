52c36cd55b181ebd8975bc34896e41eb18166be6
Samuel Nanbam Luka <samuelnanbam047@gmail.com>
2024-10-21 03:26:48 +0000

    initial commit

---
+++ constructor.js
@@ -0,0 +1,20 @@
+function registerButton(){
+    let registerButton= alert("Account register");
+ }
+ function sendButton(){
+     let deposit=alert("amount deposited successful");
+ }
+ function transferButton(){
+     let fromAccount=prompt("request account number");
+     if (fromAccount < 1000 ) alert("transfer successful") ;
+ }
+
+ function checkButton(){
+  let checkButton=prompt("account exist")
+    
+ }
+
+ function submitButton(){
+   let submit=prompt("input your password")
+   if (amount) alert (`loan request succesful`);
+ }
